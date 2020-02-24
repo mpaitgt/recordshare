89ff8919c43cbf5b1407016fb6f0b3bd359fdc0d
Matt Pignatore <mattpigs@gmail.com>
2020-02-24 12:32:10 -0500

    initializing this repo

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      default: Date.now
+    }
+  }, 
+  {
+    // disable the modification of table names; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true,
+  }
+  )
+  return User;
+}
